9fa4cb3d700bf00882f03de7c382c61dde6a102a
farhodbek235 <110379042+farhodbek235@users.noreply.github.com>
2023-09-19 07:13:47 +0500

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.Write("Enter the number of rows for the diamond: ");
+        if (int.TryParse(Console.ReadLine(), out int numRows))
+        {
+            PrintDiamond(numRows);
+        }
+
+
+
+    }
+
+    static void PrintDiamond(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int j = 0; j < 2 * i + 1; j++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+
+        for (int i = n - 2; i >= 0; i--)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int j = 0; j < 2 * i + 1; j++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
